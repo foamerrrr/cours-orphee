116be7d1ffb05ed60c16af52cdf68dadc7cac636
foamerrrr <laurentchhou3@gmail.com>
2026-03-09 16:04:03 +0100

    ArticleController + web add + articles/{id}.php

--- cours-orphee/routes/articles/{id}.php
+++ cours-orphee/routes/articles/{id}.php
@@ -1,8 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\IndexController;
 use App\Http\Controllers\ArticleController;
 
-Route::get('/', [IndexController::class, 'index']);
 Route::get('/articles/{id}', [ArticleController::class, 'show']);
