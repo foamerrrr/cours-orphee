03169b472566b7dbd53fb6cd29f7cb55b4d56f26
foamerrrr <laurentchhou3@gmail.com>
2026-03-10 11:49:30 +0100

    id dynamique dans titre details de l'article

--- cours-orphee/resources/views/pages/article-details.blade.php
+++ cours-orphee/resources/views/pages/article-details.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.app')
-@section('title', "Détails de l'article")
+@section('title', "Détails de l'article $id")
 @section('content')
 <h2> Article numéro {{$id}} <h2>
 <p>Ceci est le contenu principal de la page.</p>
