9d2c2ded9dc64d31c42578d745671d0063490727
foamerrrr <laurentchhou3@gmail.com>
2026-03-10 16:45:13 +0100

    type image/png

--- cours-orphee/resources/views/layouts/app.blade.php
+++ cours-orphee/resources/views/layouts/app.blade.php
@@ -2,7 +2,7 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="UTF-8">
-    <link rel="icon" href="{{ asset('favicon.png') }}" type="logo">
+    <link rel="icon" href="{{ asset('favicon.png') }}" type="image/png">
     <title>@yield('title')</title>
 </head>
 <body>
