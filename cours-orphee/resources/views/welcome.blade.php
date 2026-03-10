b58e39c85cbb75169d84da0329ccad2e4c73c459
foamerrrr <laurentchhou3@gmail.com>
2026-03-10 15:02:13 +0100

    break le dernier article (if loop's last, break)

--- cours-orphee/resources/views/welcome.blade.php
+++ cours-orphee/resources/views/welcome.blade.php
@@ -3,6 +3,9 @@
 @section('content')
 <h2> Bienvenue sur le site de {{ $name }} </h2>
 @foreach($articles as $article)
+    @if($loop->last)
+        @break
+    @endif
 <x-article 
     :title="$article['title']" 
     :description="$article['description']" 
